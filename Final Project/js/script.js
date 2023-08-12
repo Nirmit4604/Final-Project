66f568b976757fc60d702a099c1cb072eb163deb
Nirmit4604 <133156487+Nirmit4604@users.noreply.github.com>
2023-08-11 23:14:02 -0400

    Add files via upload

---
+++ Final Project/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var orderForm = document.querySelector('form');
+    orderForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert("Your order has been placed Successfully!");
+    });
+});
+  
